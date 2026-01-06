4b907b834593da8708c9b1b7a0b873e8f8215f95
Savio Martin <saviomartin2007@gmail.com>
2026-01-07 02:30:36 +0530

    Update sqsh.rb

--- sqsh.rb
+++ sqsh.rb
@@ -9,8 +9,7 @@ class Sqsh < Formula
   depends_on "ffmpeg"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    system "npm", "install", "-g", "--prefix", prefix
   end
 
   test do
