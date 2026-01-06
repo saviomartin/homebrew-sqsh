bb3e43ac0d18c36c9aa88f6ae5a9b3d3bb7991d7
Savio Martin <saviomartin2007@gmail.com>
2026-01-07 02:33:02 +0530

    Update sqsh.rb

--- sqsh.rb
+++ sqsh.rb
@@ -1,19 +1,19 @@
-class Sqsh < Formula
-  desc "Fast media compression for your terminal"
-  homepage "https://github.com/saviomartin/sqsh"
-  url "https://registry.npmjs.org/sqsh/-/sqsh-1.0.4.tgz"
-  sha256 "8986851c348fe21552a9641b975bec0e02d917ef3abebd38b1bc44b7b2a9b9b2"
-  license "MIT"
-
-  depends_on "node"
-  depends_on "ffmpeg"
-
-  def install
-    libexec.install Dir["*"]
-    system "npm", "install", "-g", "#{libexec}"
-  end
-
-  test do
-    system "#{bin}/sqsh", "--version"
-  end
-end
+   class Sqsh < Formula
+     desc "Fast media compression for your terminal"
+     homepage "https://github.com/saviomartin/sqsh"
+     url "https://registry.npmjs.org/sqsh/-/sqsh-1.0.4.tgz"
+     sha256 "8986851c348fe21552a9641b975bec0e02d917ef3abebd38b1bc44b7b2a9b9b2"
+     license "MIT"
+   
+     depends_on "node"
+     depends_on "ffmpeg"
+   
+     def install
+       libexec.install Dir["*"]
+       system "npm", "install", "-g", "#{libexec}"
+     end
+   
+     test do
+       system "#{bin}/sqsh", "--version"
+     end
+   end
